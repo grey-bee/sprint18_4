2e2381fd2e51561ee77b6f43877f9b9a2cae9c50
grey-bee <doctor.madbee@gmail.com>
2026-01-03 06:53:48 +0700

    Initial commit: Sprint18.4

---
+++ app/src/main/java/com/example/sprint18_4/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.sprint18_4
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sprint18_4.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var tabMediator: TabLayoutMediator
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewPager.adapter = NumbersViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when(position) {
+                0 -> tab.text = "Вкладка 1"
+                1 -> tab.text = "Вкладка 2"
+                2 -> tab.text = "Вкладка 3"
+            }
+        }
+        tabMediator.attach()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tabMediator.detach()
+    }
+
+}
---
+++ app/src/main/java/com/example/sprint18_4/NumberFragment.kt
@@ -0,0 +1,35 @@
+package com.example.sprint18_4
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sprint18_4.databinding.FragmentBinding
+
+class NumberFragment : Fragment() {
+
+    companion object {
+        private const val NUMBER = "number"
+
+        fun newInstance(number: Int) = NumberFragment().apply {
+            arguments = Bundle().apply {
+                putInt(NUMBER, number)
+            }
+        }
+    }
+
+    private lateinit var binding: FragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = FragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.number.text = requireArguments().getInt(NUMBER).toString()
+    }
+
+}
